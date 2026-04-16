173898d5434abb6307fde06cdc75f101dfab6e1b
iamjaygao <forivey@gmail.com>
2026-04-15 20:45:08 -0400

    fix: resolve migration dependency conflict between decision_slots and appointments

--- gateai/appointments/migrations/0001_initial.py
+++ gateai/appointments/migrations/0001_initial.py
@@ -8,9 +8,8 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
-    initial = True
-
     dependencies = [
+        ('decision_slots', '0005_delete_appointment_delete_appointmentrequest_and_more'),
         ('human_loop', '0002_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
